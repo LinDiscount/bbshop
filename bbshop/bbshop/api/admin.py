da5af0dc903d1c65b407a04330df9846049f11ae
LinDiscount <146705689+LinDiscount@users.noreply.github.com>
2024-02-10 13:31:10 +0300

    Add files via upload

---
+++ bbshop/bbshop/api/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Services, Orders
+
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_of_order', 'services', 'description', 'owner')
+    list_display_links = ('id', 'date_of_order', 'services', 'description', 'owner')
+
+
+
+
